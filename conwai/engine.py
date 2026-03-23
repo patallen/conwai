05f9383347b06ddb8a8e2f19694933db388b0a5e
Patrick Allen <prallen90@gmail.com>
2026-03-22 23:58:30 -0400

    Emit TickStarted/TickEnded from Engine.tick() and drain bus between systems

--- conwai/engine.py
+++ conwai/engine.py
@@ -37,8 +37,23 @@ class Engine:
         tick = self.world.get_resource(TickNumber)
         tick.value += 1
         log.info(f"[ENGINE] tick {tick.value}")
+
+        bus = self.world._bus
+
+        if bus:
+            from conwai.event_types import TickStarted
+            bus.emit(TickStarted(tick=tick.value))
+            bus.drain()
+
         for system in self._systems:
             await system.run(self.world)
+            if bus:
+                bus.drain()
+
+        if bus:
+            from conwai.event_types import TickEnded
+            bus.emit(TickEnded(tick=tick.value))
+            bus.drain()
 
         self.world.flush()
         self.world.save_metadata("tick", {"value": tick.value})
